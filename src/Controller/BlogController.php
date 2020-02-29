e5178c188d535fe7fd45eae95db4767a4651e200
Axel Urbain <axelurbain@mbp-de-axel.home>
2020-02-29 16:59:40 +0100

    home list article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,11 +10,19 @@ class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
+     * @param EntityManagerInterface $em
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(EntityManagerInterface $em)
     {
+        $article = $em->getRepository('App:Article')->findBy(
+            array('published' => true),
+            array('created_at' => 'desc')
+        );
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $article
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,10 +13,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(EntityManagerInterface $em)
     {
+        $article = $em->getRepository('App:Article')->findBy(
+            array('published' => true),
+            array('created_at' => 'desc'),
+            3
+        );
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'articles' => $article
         ]);
     }
 }
